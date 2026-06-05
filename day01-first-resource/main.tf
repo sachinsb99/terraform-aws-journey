d27104eb151e21a819120b49af4b2ead52b973bc
sachinsb99 <132380185+sachinsb99@users.noreply.github.com>
2026-06-05 18:10:32 +0530

    Create main.tf

---
+++ day01-first-resource/main.tf
@@ -0,0 +1,18 @@
+resource "aws_s3_bucket" "first_bucket" {
+  bucket = "terraform-learning-day01-yourname-2024"
+  
+  tags = {
+    Name        = "TerraformFirstBucket"
+    Environment = "Learning"
+    Day         = "Day01"
+  }
+}
+# Block public access on the bucket (best practice)
+resource "aws_s3_bucket_public_access_block" "first_bucket_pab" {
+  bucket = aws_s3_bucket.first_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
