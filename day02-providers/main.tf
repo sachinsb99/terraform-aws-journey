3142b7728baf3fdcc337b42bbf89d2f6c1af55dd
Ubuntu <ubuntu@ip-172-31-46-191.ap-south-1.compute.internal>
2026-06-05 13:06:05 +0000

    day 2

---
+++ day02-providers/main.tf
@@ -0,0 +1,28 @@
+resource "aws_s3_bucket" "first_bucket" {
+  # bucket name must be globally unique across ALL of AWS
+  bucket = "terraform-learning-day01-yourname-2024"
+
+  tags = {
+    Name        = "TerraformFirstBucket"
+    Environment = "Learning"
+    Day         = "Day01"
+  }
+}
+resource "aws_s3_bucket" "log_bucket" {
+  bucket = "terraform-learning-logs-yourname-2024"
+  
+  tags = {
+    Name    = "LogBucket"
+    Purpose = "Logging"
+  }
+}
+
+# Block public access on the bucket (best practice)
+resource "aws_s3_bucket_public_access_block" "first_bucket_pab" {
+  bucket = aws_s3_bucket.first_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
